6c61d89c6ab396b5a947e158b1b72303923e3d85
Sebastian Matuszczyk <matuszczyksebastian9@gmail.com>
2021-12-12 20:12:41 +0100

    indents fix

--- InfraRed.h
+++ InfraRed.h
@@ -1,16 +1,16 @@
 #ifndef _INFRARED_h
 #define _INFRARED_h
 
-#define ir0			0xE916FF00
-#define ir1			0xF30CFF00
-#define ir2			0xE718FF00
-#define ir3			0xA15EFF00
-#define ir4			0xF708FF00
-#define ir5			0xE31CFF00
-#define ir6			0xA55AFF00
-#define ir7			0xBD42FF00
-#define ir8			0xAD52FF00
-#define ir9			0xB54AFF00
+#define ir0		0xE916FF00
+#define ir1		0xF30CFF00
+#define ir2		0xE718FF00
+#define ir3		0xA15EFF00
+#define ir4		0xF708FF00
+#define ir5		0xE31CFF00
+#define ir6		0xA55AFF00
+#define ir7		0xBD42FF00
+#define ir8		0xAD52FF00
+#define ir9		0xB54AFF00
 #define irMinus		0xF807FF00
 #define irPlus		0xEA15FF00
 #define irEQ		0xF609FF00
@@ -18,7 +18,7 @@
 #define irNext		0xBF40FF00
 #define irPlay		0xBC43FF00
 
-// Zarezerwowane adresy, ale nie maj¹ zastosowania póki co
+// Zarezerwowane adresy, ale nie maja zastosowania pÃ³ki co
 #define irCHminus	0xBA45FF00
 #define irCHplus	0xB847FF00
 #define irCH		0xB946FF00
@@ -32,10 +32,10 @@ private:
 public:
 
 	InfraRed(int IR_RECEIVE_PIN);
-	void begin();				// Rozpoczyna prace z czujnikiem
-	void resume();				// Wznawia prace czujnika
-	bool available();			// Sprawdza, czy pojawil sie jakis sygnal
-	char decode();				// Dekoduje pobrany sygnal na akcje
+	void begin();		// Rozpoczyna prace z czujnikiem
+	void resume();		// Wznawia prace czujnika
+	bool available();	// Sprawdza, czy pojawil sie jakis sygnal
+	char decode();		// Dekoduje pobrany sygnal na akcje
 };
 
 
